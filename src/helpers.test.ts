d3e7747d70e31d3dca3cf1db851df351421facc1
Massimo Iacolare <info@massimoiacolare.it>
2023-01-24 17:54:28 +0100

    make jest work with ESM modules

--- src/helpers.test.ts
+++ src/helpers.test.ts
@@ -1,5 +1,7 @@
 import { expect, test } from "@jest/globals"
+import { delay } from "./helpers.js"
 
-test("it works", () => {
+test("it works", async () => {
+    await delay(100)
     expect("a").toBe("a")
 })
