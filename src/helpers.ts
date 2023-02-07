7c50429c7db85444b6407c368ff6bf7d2508e5d1
Massimo Iacolare <info@massimoiacolare.it>
2023-02-07 18:54:48 +0100

    add pretty() utility

--- src/helpers.ts
+++ src/helpers.ts
@@ -1,1 +1,4 @@
-export const delay = (ms: number): Promise<number> => new Promise((resolve) => setTimeout(() => resolve(ms), ms))
+export const delay = (ms: number): Promise<number> =>
+    new Promise((resolve) => setTimeout(() => resolve(ms), ms))
+
+export const pretty = (obj: unknown) => JSON.stringify(obj, null, 2)
