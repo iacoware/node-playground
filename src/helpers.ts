c5761465c0d2efd37f39576bb5a55e3d5d1b7083
Massimo Iacolare <info@massimoiacolare.it>
2023-02-10 18:29:43 +0100

    added some helper to color output

--- src/helpers.ts
+++
@@ -1,4 +0,0 @@
-export const delay = (ms: number): Promise<number> =>
-    new Promise((resolve) => setTimeout(() => resolve(ms), ms))
-
-export const pretty = (obj: unknown) => JSON.stringify(obj, null, 2)
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,13 @@
-import { delay } from "./helpers"
+import { delay, green, yellow } from "./support/helpers"
 
 const hello = (who: string) => `Hello ${who}!`
 
 const greet = async () => {
     const result = await delay(200)
     const message = hello("World")
-    console.log(`${message} after ${result}ms`)
+    const duration = `${result}ms`
+
+    console.log(`${green(message)} after ${yellow(duration)}`)
 }
 
 greet()
